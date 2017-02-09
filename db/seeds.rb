5e74159a6cc77d65b09e605a2988de5c5aec8304
Rafael Millan <rafmillan@gmail.com>
2017-02-09 17:24:05 +0100

    Fix seed uniqueness

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ Dose.destroy_all
 Ingredient.destroy_all
 Cocktail.destroy_all
 
+Faker::UniqueGenerator.clear
+
 30.times do
   Ingredient.create!(name: Faker::Food.unique.ingredient)
 end
@@ -9,13 +11,13 @@ end
 10.times do
   cocktail = Cocktail.new(name: Faker::Space.unique.galaxy)
   3.times do
-    ingredient = Ingredient.all.sample
+    ingredient = Ingredient.all.sample.name
     until cocktail.ingredients.find_by(name: ingredient).nil?
-      ingredient = Ingredient.all.sample
+      ingredient = Ingredient.all.sample.name
     end
     dose = Dose.new(
       description: Faker::Food.measurement,
-      ingredient: Ingredient.all.sample,
+      ingredient: Ingredient.find_by(name: ingredient),
       cocktail: cocktail
     )
     dose.save!
